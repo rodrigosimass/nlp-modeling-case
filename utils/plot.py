3af22f963c1b9844b082d607aa852a3ebb94eb08
rodrigosimass <rodrigo_simas10@hotmail.com>
2023-05-09 18:36:51 +0200

    comparisson of simple model with baseline

---
+++ utils/plot.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+
+def plot_conf_matrix(y_test, y_pred):
+    conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+
+    _, ax = plt.subplots(figsize=(5, 5))
+    ax.matshow(conf_matrix, cmap=plt.cm.Oranges, alpha=0.3)
+    for i in range(conf_matrix.shape[0]):
+        for j in range(conf_matrix.shape[1]):
+            ax.text(x=j, y=i,s=conf_matrix[i, j], va='center', ha='center', size='xx-large')
+    
+    plt.xlabel('Predictions', fontsize=18)
+    plt.ylabel('Actuals', fontsize=18)
+    plt.title('Confusion Matrix', fontsize=18)
+    plt.show()
